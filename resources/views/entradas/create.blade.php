65fd6548a09bce466e38f0a7dcc78645420eb8dc
Rebecca Pineda <rebeccapineda@MacBook-Pro-de-Rebecca.local>
2020-11-28 16:20:51 -0600

    diseño formularios

--- resources/views/entradas/create.blade.php
+++ resources/views/entradas/create.blade.php
@@ -1,9 +1,38 @@
-@extends('layouts.plantilla')
+@extends('layouts.plantillaMenu')
 
 @section('title', 'Registrar Entrada')
 
 @section('content')
 
     <h1>Registra tu entrada</h1>
+
+    <form action="">
+        <div class="form-group">
+        <label for="">Tipo de entrada</label>
+        <input type="text"
+            class="form-control" name="tipoEntrada" placeholder="Ej. Salario quincenal">
+        </div>
+
+        <div class="form-group">
+            <label for="">Monto</label>
+            <input type="number"
+                class="form-control" name="montoEntrada" placeholder="$0.00">
+        </div>
+
+        <div class="form-group">
+            <label for="">Fecha</label>
+            <input type="date"
+                class="form-control" name="fechaEntrada" placeholder="">
+        </div>
+
+        <div class="form-group">
+            <label for="">Adjunta comprobante</label>
+            <input type="file"
+                class="form-control" name="fotoEntrada" placeholder="">
+        </div>
+
+        <button type="submit" name="crearEntrada" class="btn btn-primary btn-lg btn-block">Registrar Entrada</button>
+
+    </form>
     
 @endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,5 +5,23 @@
 @section('content')
 
     <h1>Login</h1>
+
+    <form action="">
+
+        <div class="form-group">
+        <label for="">Usuario</label>
+        <input type="text"
+            class="form-control" name="tipo" placeholder="Nombre de Usuario">
+        </div>
+
+        <div class="form-group">
+            <label for="">Contraseña</label>
+            <input type="password"
+                class="form-control" name="tipo" placeholder="Ingresa contraseña">
+        </div>
+
+        <button type="submit" name="login" class="btn btn-primary btn-lg btn-block">Iniciar Sesión</button>
+
+    </form>
     
 @endsection
